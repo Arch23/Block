5ebac7e253c40660481cc000711f1d686375871a
VoiderX <gabrieldeabreu.ti@gmail.com>
2016-10-21 01:38:20 -0200

    Verificações adicionais

--- web/model/ReservaDAO.php
+++ web/model/ReservaDAO.php
@@ -3,12 +3,97 @@
 	class  ReservaDAO{ //Classe para controle das reservas
 		public $conn; //Conexão do usuário
 
-		function __construct($username,$password){ //Passa username e senha do usuário quando a classe for instanciada
+	 	function __construct($username,$password){ //Passa username e senha do usuário quando a classe for instanciada
 			$servername="localhost";
 			$dbname="roomz";
 			$this->conn = new mysqli($servername, $username, $password,$dbname); //Cria a conexão do usuário dentro da classe                 
 
 		}
+		function verificaHorario(){
+			$h0730= new DateTime('07:30:00');
+			$h0820= new DateTime('08:20:00');
+			$h0910= new DateTime('09:10:00');
+			$h1000= new DateTime('10:00:00');
+			$h1020= new DateTime('10:20:00');
+			$h1110= new DateTime('11:10:00');
+			$h1200= new DateTime('12:00:00');
+
+			$h1250= new DateTime('12:50:00');
+			$h1300= new DateTime('13:00:00');
+			$h1350= new DateTime('13:50:00');
+			$h1440= new DateTime('14:40:00');
+			$h1530= new DateTime('15:30:00');
+			$h1550= new DateTime('15:50:00');
+			$h1640= new DateTime('16:40:00');
+			$h1730= new DateTime('17:30:00');
+			$h1750= new DateTime('17:50:00');
+
+			$h1840= new DateTime('18:40:00');
+			$h1930= new DateTime('19:30:00');
+			$h2020= new DateTime('20:20:00');
+			$h2110= new DateTime('21:10:00');
+			$h2120= new DateTime('21:20:00');
+			$h2210= new DateTime('22:10:00');
+			$h2300= new DateTime('23:00:00');
+
+			$now= new DateTime('now');
+			$codhorario="teste";
+			if ($h0730 <= $now && $now <= $h0820 ) {
+			    $codhorario="M1";
+			}
+			else if($h0820 <= $now && $now <=$h0910){
+				$codhorario="M2";
+			}
+			else if($h0910 <= $now && $now <=$h1020){
+				$codhorario="M3";
+			}
+			else if($h1020 <= $now && $now <=$h1110){
+				$codhorario="M4";
+			}
+			else if($h1110 <= $now && $now <=$h1200){
+				$codhorario="M5";
+			}
+
+			else if($h1200 <= $now && $now <=$h1300){
+				$codhorario="M6";
+			}	
+			else if($h1300 <= $now && $now <=$h1350){
+				$codhorario="T1";
+			}
+			else if($h1350 <= $now && $now <=$h1440){
+				$codhorario="T2";
+			}
+			else if($h1440 <= $now && $now <=$h1550){
+				$codhorario="T3";
+			}
+			else if($h1550 <= $now && $now <=$h1640){
+				$codhorario="T4";
+			}
+			else if($h1640 <= $now && $now <=$h1730){
+				$codhorario="T5";
+			}
+			else if($h1730 <= $now && $now <=$h1840){
+				$codhorario="T6";
+			}
+			else if($h1840 <= $now && $now <=$h1930){
+				$codhorario="N1";
+			}
+			else if($h1930 <= $now && $now <=$h2020){
+				$codhorario="N2";
+			}
+			else if($h2020 <= $now && $now <=$h2120){
+				$codhorario="N3";
+			}
+			else if($h2120 <= $now && $now <=$h2210){
+				$codhorario="N4";
+			}
+			else if($h2210 <= $now && $now <=$h2300){
+				$codhorario="N5";
+			}else{
+				$codhorario="N6";
+			}
+			return $codhorario;
+		}  
 
 		function retornaBlocos(){  //Insere os blocos no list na view
 				$sql="SELECT NOME_BLOCO FROM BLOCO";
@@ -27,6 +112,7 @@
 		}
 
 		function retornaReservaNormal($Bloco,$Andar,$Sala,$Datadia,$coduser){	//Retorna as reservas criando a tabela na view		
+			$codhorario= $this->verificaHorario();
 			$coduser=substr($coduser, 1); //Retira o a do nome de usuário
 			$dia=substr($Datadia, 0,2); //Pega o dia 
 			$mes=substr($Datadia,3,2); //Pega o mes
@@ -104,11 +190,52 @@
 									$Resultf=$this->conn->query($sqlf);
 								    //Se nenhum dos campos tiver reserva considera que a sala está disponível
 									if(($Result->num_rows==0) && ($Resultf->num_rows==0)){
-									if($date>=date("Y-m-d")){								
-										echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
-									}else{
-										echo '<td style="color:gray;">INDISPONÍVEL</td>';
-									}
+										if($date>=date("Y-m-d")){
+												if($date==date("Y-m-d")){
+													if($codhorario!="N6"){
+													if(substr($codhorario,0,1)=='M'){
+															if($letra=='M'){
+																if($j>=substr($codhorario,1,1)){					
+																	echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+																}else{
+																	echo '<td style="color:gray;">INDISPONÍVEL</td>';	
+																}
+															}
+															else{
+																echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';	
+															}
+														}
+														else if(substr($codhorario,0,1)=='T'){
+															if($letra=='N'){
+																echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+															}
+															else if(($letra=='T') AND ($j>=substr($codhorario,1,1))){
+																echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+															}
+															else{
+																echo '<td style="color:gray;">INDISPONÍVEL</td>';	
+															}
+														}
+														else if($letra=='N' AND substr($codhorario,0,1)=='N'){
+															if($j>=substr($codhorario,1,1)){
+																echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+															}
+															else{
+																echo '<td style="color:gray;">INDISPONÍVEL</td>';	
+															}
+														}
+														else{
+															echo '<td style="color:gray;">INDISPONÍVEL</td>';
+														}
+													}else{
+														echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+													}
+												}else{
+													echo '<td style="color:blue;">'.$letra.$j." ".$date.'</td>';
+												}
+										}else{
+											echo '<td style="color:gray;">INDISPONÍVEL</td>';
+										}
 									}else{ //Verifica se a reserva é daquele usuário ou se é de outro
 										 $sqlu= "SELECT HORARIO_ID_HORARIO FROM RESERVA, BLOCO WHERE ID_BLOCO_RESERVA=BLOCO.ID_BLOCO AND '".$Bloco."'=BLOCO.NOME_BLOCO AND DATA_RESERVA='$date' AND ID_SALA_RESERVA=$Sala AND ID_ANDAR_RESERVA=$Andar AND HORARIO_ID_HORARIO='".$letra.$j."' AND COD_USUARIO_RESERVA=".$coduser;
 									     $Resultu=$this->conn->query($sqlu);
--- web/model/UserDAO.php
+++ web/model/UserDAO.php
@@ -1,7 +1,94 @@
 <?php
+    date_default_timezone_set ("America/Sao_Paulo");
 	class UserDAO{ //Classe para a conexão e criação(cadastro de um usuário)
                 public $conn; //Conexão do usuário
                 public $username;
+
+        function verificaHorario(){
+            $h0730= new DateTime('07:30:00');
+            $h0820= new DateTime('08:20:00');
+            $h0910= new DateTime('09:10:00');
+            $h1000= new DateTime('10:00:00');
+            $h1020= new DateTime('10:20:00');
+            $h1110= new DateTime('11:10:00');
+            $h1200= new DateTime('12:00:00');
+
+            $h1250= new DateTime('12:50:00');
+            $h1300= new DateTime('13:00:00');
+            $h1350= new DateTime('13:50:00');
+            $h1440= new DateTime('14:40:00');
+            $h1530= new DateTime('15:30:00');
+            $h1550= new DateTime('15:50:00');
+            $h1640= new DateTime('16:40:00');
+            $h1730= new DateTime('17:30:00');
+            $h1750= new DateTime('17:50:00');
+
+            $h1840= new DateTime('18:40:00');
+            $h1930= new DateTime('19:30:00');
+            $h2020= new DateTime('20:20:00');
+            $h2110= new DateTime('21:10:00');
+            $h2120= new DateTime('21:20:00');
+            $h2210= new DateTime('22:10:00');
+            $h2300= new DateTime('23:00:00');
+
+            $now= new DateTime('now');
+            $codhorario="teste";
+            if ($h0730 <= $now && $now <= $h0820 ) {
+                $codhorario="M1";
+            }
+            else if($h0820 <= $now && $now <=$h0910){
+                $codhorario="M2";
+            }
+            else if($h0910 <= $now && $now <=$h1020){
+                $codhorario="M3";
+            }
+            else if($h1020 <= $now && $now <=$h1110){
+                $codhorario="M4";
+            }
+            else if($h1110 <= $now && $now <=$h1200){
+                $codhorario="M5";
+            }
+
+            else if($h1200 <= $now && $now <=$h1300){
+                $codhorario="M6";
+            }   
+            else if($h1300 <= $now && $now <=$h1350){
+                $codhorario="T1";
+            }
+            else if($h1350 <= $now && $now <=$h1440){
+                $codhorario="T2";
+            }
+            else if($h1440 <= $now && $now <=$h1550){
+                $codhorario="T3";
+            }
+            else if($h1550 <= $now && $now <=$h1640){
+                $codhorario="T4";
+            }
+            else if($h1640 <= $now && $now <=$h1730){
+                $codhorario="T5";
+            }
+            else if($h1730 <= $now && $now <=$h1840){
+                $codhorario="T6";
+            }
+            else if($h1840 <= $now && $now <=$h1930){
+                $codhorario="N1";
+            }
+            else if($h1930 <= $now && $now <=$h2020){
+                $codhorario="N2";
+            }
+            else if($h2020 <= $now && $now <=$h2120){
+                $codhorario="N3";
+            }
+            else if($h2120 <= $now && $now <=$h2210){
+                $codhorario="N4";
+            }
+            else if($h2210 <= $now && $now <=$h2300){
+                $codhorario="N5";
+            }else{
+                $codhorario="N6";
+            }
+            return $codhorario;
+        }  
 
 		function conectaBanco($servername,$username,$password,$dbname){ 
 			$conn = new mysqli($servername, $username, $password,$dbname);                       
@@ -80,21 +167,65 @@
         }
         function retornaReservasUser(){
             $cont=0;
+            $codhorario=$this->verificaHorario();
             $coduser=substr($this->username, 1); //Retira o a do nome de usuário
-            $result=$this->conn->query("SELECT * FROM RESERVA WHERE COD_USUARIO_RESERVA=$coduser AND DATA_RESERVA>='".date("Y-m-d")."'");
+            $result=$this->conn->query("SELECT * FROM RESERVA WHERE COD_USUARIO_RESERVA=$coduser AND DATA_RESERVA>='".date("Y-m-d")."'
+                 ORDER BY DATA_RESERVA,CASE WHEN HORARIO_ID_HORARIO LIKE 'M%' THEN 1 WHEN HORARIO_ID_HORARIO LIKE 'T%' THEN 2 ELSE 3 END;");
             while($row=$result->fetch_assoc()){
-            $cont++;         
-            echo' <div id="Reserva'.$cont.'"class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 id="Reserva'.$cont.'sala" class="panel-title">Sala reservada:'.$row["ID_BLOCO_RESERVA"].$row["ID_ANDAR_RESERVA"].$row["ID_SALA_RESERVA"].'</h3>
-                  </div>
-                  <div  class="panel-body">
-                    <p id="Reserva'.$cont.'data" class="data-reserva">Data:'.$row["DATA_RESERVA"].'</p>
-                    <p id="Reserva'.$cont.'horario" class="hora-reserva">Horário:'.$row["HORARIO_ID_HORARIO"].'</p>
-                    <button id="Reserva'.$cont.'" class="round-button"> X</button>
-                </div>
-        </div>';
+            $cont++;
+            if($row["DATA_RESERVA"]!=date("Y-m-d")){        
+                $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+              }else{
+                    if(substr($codhorario,0,1)=='M'){
+                        if(substr($row["HORARIO_ID_HORARIO"], 0,1)=='M'){
+                            if(substr($row["HORARIO_ID_HORARIO"], 1,1)>=substr($codhorario,1,1)){
+                              $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                            }
+                        }else{
+                            $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                        }
+                    }
+                    else if(substr($codhorario, 0,1)=='T'){
+                        if(substr($row["HORARIO_ID_HORARIO"], 0,1)=='M'){
+
+                        }
+                        else if(substr($row["HORARIO_ID_HORARIO"], 0,1)=='T'){
+                            if(substr($row["HORARIO_ID_HORARIO"], 1,1)>=substr($codhorario,1,1)){
+                              $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                            }
+                            }else{
+                            $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                            }                      
+                        }
+
+                        else if(substr($codhorario,0,1)=='N'){
+                            if(substr($codhorario,0,1)=='M' || substr($codhorario,0,1)=='T'){
+                                $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                            }else{
+                                if(substr($codhorario,0,1)=='N'){
+                                    if(substr($row["HORARIO_ID_HORARIO"], 1,1)>=substr($codhorario,1,1)){
+                                         $this->enviaDiv($cont,$row["ID_BLOCO_RESERVA"],$row["ID_ANDAR_RESERVA"],$row["ID_SALA_RESERVA"],$row["DATA_RESERVA"],$row["HORARIO_ID_HORARIO"]);
+                                    }else{
+
+                                    }
+                                }
+                            }                   
+                        }          
+                }
             }
+        }
+
+        function enviaDiv($cont,$bloco,$sala,$andar,$data,$horario){
+            echo' <div id="Reserva'.$cont.'"class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 id="Reserva'.$cont.'sala" class="panel-title">Sala reservada:'.$bloco.$andar.$sala.'</h3>
+                      </div>
+                      <div  class="panel-body">
+                        <p id="Reserva'.$cont.'data" class="data-reserva">Data:'.$data.'</p>
+                        <p id="Reserva'.$cont.'horario" class="hora-reserva">Horário:'.$horario.'</p>
+                        <button id="Reserva'.$cont.'" class="round-button"> X</button>
+                    </div>
+            </div>';
         }
 	}				
 	?>
